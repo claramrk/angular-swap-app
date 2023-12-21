1d9fe0aea16f942247371cad57c00adf762e68e1
Clara Mrkos <mrkos.clara@gmail.com>
2023-12-21 11:11:02 +0100

    Add new trial code

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,10 +1,11 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './test.component.html',
   styleUrl: './test.component.css',
 })
@@ -12,4 +13,9 @@ export class TestComponent {
   onClick() {
     console.log('clicked');
   }
+  logInputValue(inputValue: string): void {
+    console.log('Eingabewert:', inputValue);
+  }
+
+  items: Array<string> = ['Apfel', 'Banane', 'Orange', 'Traube'];
 }
